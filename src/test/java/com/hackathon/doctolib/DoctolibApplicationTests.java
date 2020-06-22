c2ddb6c4c661ae6913ea3fda2f451465126691fb
Xavier <x.crochet@gmail.com>
2020-06-22 14:18:51 +0200

    initial commit

---
+++ src/test/java/com/hackathon/doctolib/DoctolibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.doctolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
